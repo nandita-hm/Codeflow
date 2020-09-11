0a77fbe3ac66a3d4c65822601391665331c0bd8c
anja-li <54656887+anja-li@users.noreply.github.com>
2020-09-12 00:59:37 +0530

    update

--- app/src/main/java/com/example/codeflow/MainActivity.java
+++ app/src/main/java/com/example/codeflow/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.codeflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
