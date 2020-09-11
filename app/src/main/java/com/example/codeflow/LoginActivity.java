75f2c2af27be3afccce0412bb8167127e3868d69
anja-li <54656887+anja-li@users.noreply.github.com>
2020-09-12 01:02:48 +0530

    update

---
+++ app/src/main/java/com/example/codeflow/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.codeflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
