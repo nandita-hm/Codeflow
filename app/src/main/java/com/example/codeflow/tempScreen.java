a2feeb1d2fc9c03ce3af6129f7f5e9f31942dc7d
Nandita M <40920879+nandita-hm@users.noreply.github.com>
2020-09-12 18:33:49 +0530

    Release variant added

--- app/src/main/java/com/example/codeflow/tempScreen.java
+++ app/src/main/java/com/example/codeflow/tempScreen.java
@@ -1,23 +1,14 @@
 package com.example.codeflow;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class tempScreen extends AppCompatActivity {
-    static String username;
-    TextView textView;
-    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_screen);
-        userId = getIntent().getStringExtra("USER_ID");
-        textView=findViewById(R.id.name);
-        textView.setText("Hello "+userId);
-
-
     }
 }
