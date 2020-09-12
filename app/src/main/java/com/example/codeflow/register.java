6f31f05db770a411ff9ea999b61aa1763b086452
GandhaliShinde <gandhalishinde27@gmail.com>
2020-09-12 22:40:52 +0530

    firebase auth added

--- app/src/main/java/com/example/codeflow/register.java
+++ app/src/main/java/com/example/codeflow/register.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,14 +13,21 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.Calendar;
+
 
 public class register extends AppCompatActivity {
     private static final String TAG = "Register";
 
-
+    FirebaseAuth fAuth;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
@@ -77,6 +85,16 @@ public class register extends AppCompatActivity {
         Button reg= findViewById(R.id.register);
         final EditText username= findViewById(R.id.username);
         final EditText email= findViewById(R.id.email);
+
+        fAuth=FirebaseAuth.getInstance();
+
+        // IF USER IS ALREADY CREATED
+        if(fAuth.getCurrentUser() !=null){
+
+            Toast.makeText(register.this, "User already registered.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
+
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,11 +102,37 @@ public class register extends AppCompatActivity {
                 String s=email.getText().toString();
                 String password=confirmpass.getText().toString();
                 String user=username.getText().toString();
+
+                if(TextUtils.isEmpty(user))
+                {
+                    username.setError("Username is Required");
+                    return;
+                }
+
+                //REGISTER USER INTO DATABASE
+
+                fAuth.createUserWithEmailAndPassword(s,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //CHECKING WHETHER REGISTRATION SUCCESS OR NOT
+
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            // IF REGISTERED, GO TO 'login'
+                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(register.this, "User creation failed, try again.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
             }
         });
-
-
     }
+
     public void onClickBack(View view){
         Intent back= new Intent(register.this,LoginActivity.class);
         startActivity(back);
