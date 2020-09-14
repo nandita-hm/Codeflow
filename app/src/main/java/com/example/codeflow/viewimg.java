0016f24215397ecf9bb00167291c866f44227213
anja-li <54656887+anja-li@users.noreply.github.com>
2020-09-14 13:10:16 +0530

    viewimg done!

--- app/src/main/java/com/example/codeflow/viewimg.java
+++ app/src/main/java/com/example/codeflow/viewimg.java
@@ -2,9 +2,16 @@ package com.example.codeflow;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FileDownloadTask;
@@ -12,9 +19,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,13 +36,17 @@ public class viewimg extends AppCompatActivity {
     DatabaseReference reference;
     ImageView imgv;
     TextView tn;
+    Toolbar toolbar;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewimg);
-        imgv=findViewById(R.id.imgv);
-        tn=findViewById(R.id.tee);
+        imgv = findViewById(R.id.imgv);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("MindMaps will be viewed here!");
+        tn = findViewById(R.id.tee);
         tn.setText("TeamName:");
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReferenceFromUrl("gs://helloworld-94b6e.appspot.com").child("IMG20190226183808.jpg");
@@ -51,10 +64,59 @@ public class viewimg extends AppCompatActivity {
                 public void onFailure(@NonNull Exception exception) {
                 }
             });
-        } catch (IOException e ) {
+        } catch (IOException e) {
             Toast.makeText(this, " exception", Toast.LENGTH_SHORT).show();
         }
+        toolbar.inflateMenu(R.menu.menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.delete:
+                        // addSomething();
+                        return true;
+                    case R.id.download:
+                        // startSettings();
+                        return true;
+                    case R.id.logout:
+                        signOut();
+
+                    default:
+                        return false;
+                }
+            }
+        });
 
 
+    }
+
+    private void signOut() {
+
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        FirebaseAuth.getInstance().signOut();
+                        Toast.makeText(getApplicationContext(), "Logged Out", Toast.LENGTH_SHORT).show();
+                        Intent mainIntent = new Intent(viewimg.this, LoginActivity.class);
+                        viewimg.this.startActivity(mainIntent);
+                        viewimg.this.finish();
+                    }
+                });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        mGoogleApiClient.connect();
+        super.onStart();
     }
 }
